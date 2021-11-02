cc9ca369dc9d109dd6f1d1f19f21337a9fd9491a
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2021-11-02 14:25:29 +0200

    [Service/Repository] Implement contact service/repository layer

--- src/main/java/com/mlgmag/contacts/service/ContactService.java
+++ src/main/java/com/mlgmag/contacts/service/ContactService.java
@@ -1,14 +1,31 @@
 package com.mlgmag.contacts.service;
 
 import com.mlgmag.contacts.entity.Contact;
+import com.mlgmag.contacts.repository.ContactRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.apache.commons.lang3.BooleanUtils.isFalse;
 
 @Service
 public class ContactService {
+
+    private final ContactRepository contactRepository;
+
+    public ContactService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
     public List<Contact> getContactsByNameFilter(String nameFilter) {
-        return Collections.emptyList();
+        return contactRepository.findAll().stream()
+                .filter(contact -> contact.getName() != null)
+                .filter(contact -> isFalse(isNameMatchRegEx(contact.getName(), nameFilter)))
+                .collect(Collectors.toList());
+    }
+
+    private boolean isNameMatchRegEx(String name, String regEx) {
+        return name.matches(regEx);
     }
 }
