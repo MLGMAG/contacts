a2251678337bca979d824d0273700d67011eea65
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2021-11-02 15:11:25 +0200

    [Batches] Refactor 'getContactsByNameFilter', so that it can process big amount of data

--- src/main/java/com/mlgmag/contacts/service/ContactService.java
+++ src/main/java/com/mlgmag/contacts/service/ContactService.java
@@ -2,8 +2,11 @@ package com.mlgmag.contacts.service;
 
 import com.mlgmag.contacts.entity.Contact;
 import com.mlgmag.contacts.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,6 +15,9 @@ import static org.apache.commons.lang3.BooleanUtils.isFalse;
 @Service
 public class ContactService {
 
+    @Value("${db.batch.size}")
+    private int batchSize;
+
     private final ContactRepository contactRepository;
 
     public ContactService(ContactRepository contactRepository) {
@@ -19,9 +25,24 @@ public class ContactService {
     }
 
     public List<Contact> getContactsByNameFilter(String nameFilter) {
-        return contactRepository.findAll().stream()
+
+        int rowsCount = (int) contactRepository.count();
+
+        List<Contact> result = new ArrayList<>();
+
+        int pages = (rowsCount / batchSize) + 1;
+        for (int i = 0; i < pages; i++) {
+            List<Contact> batchContacts = contactRepository.findAll(PageRequest.of(i, batchSize)).getContent();
+            result.addAll(filterContactsByNames(batchContacts, nameFilter));
+        }
+
+        return result;
+    }
+
+    private List<Contact> filterContactsByNames(List<Contact> contacts, String regEx) {
+        return contacts.stream()
                 .filter(contact -> contact.getName() != null)
-                .filter(contact -> isFalse(isNameMatchRegEx(contact.getName(), nameFilter)))
+                .filter(contact -> isFalse(isNameMatchRegEx(contact.getName(), regEx)))
                 .collect(Collectors.toList());
     }
 
